a3713dd7f38e3ad064ce9fa442b31d06a648e29c
Aim <7asim6@gmail.com>
2025-06-29 19:11:18 +0400

    Report elave olundu

--- piknik_qeydiyyat/__manifest__.py
+++ piknik_qeydiyyat/__manifest__.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Piknikə Giriş Modulu",
-    'summary': """
-        Piknik ərazisinə daxil olan şəxslərin və maşınların qeydiyyatı.
-    """,
-
+    'summary': "Piknik ərazisinə daxil olan şəxslərin və maşınların qeydiyyatı.",
     'author': "Asim",
     'website': "https://github.com/7aim/odoo-custom-modules/tree/main/piknik_qeydiyyat",
     'category': 'Uncategorized',
-    'version': '1.0',
+    'version': '1.1',
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'report/picnic_reports.xml',
+        'report/picnic_report_templates.xml',
         'views/picnic_entry_views.xml',
-        'views/picnic_zone_views.xml', 
+        'views/picnic_zone_views.xml',
         'views/accepting_person_views.xml',
     ],
     'installable': True,
