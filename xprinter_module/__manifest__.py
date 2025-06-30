abb4f506a324f945e1daab45f58a1e863e0c334d
Aim <7asim6@gmail.com>
2025-06-30 12:24:51 +0400

    -

--- xprinter_module/__manifest__.py
+++ xprinter_module/__manifest__.py
@@ -1,17 +1,16 @@
 {
-    'name': 'XPrinter Integration',
+    'name': 'XPrinter POS Integration',
     'version': '1.0',
     'category': 'Point of Sale',
-    'summary': 'XPrinter üçün POS çap modulu',
-    'description': """X-Printer ESC/POS dəstəyi""",
+    'summary': 'Adds XPrinter support to POS via a direct printing service',
+    'description': """X-Printer ESC/POS desteği""",
     'author': 'Sahib Akbar',
-    'depends': ['point_of_sale'],
-    'data': [],
+    'depends': ['point_of_sale', 'direct_print_interface'], # direct_print_interface'i ekleyin
     'assets': {
         'point_of_sale.assets': [
             'xprinter_module/static/src/js/xprinter.js',
         ],
     },
     'installable': True,
-    'application': True,
+    'application': True, # Bunu False yapın
 }
