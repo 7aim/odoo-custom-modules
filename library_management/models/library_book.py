280f902f6dc2e52f9b32249b6aac67ec117392f8
Aim <7asim6@gmail.com>
2025-07-08 09:31:53 +0400

    Demo 1.2

--- library_management/models/library_book.py
+++ library_management/models/library_book.py
@@ -9,8 +9,10 @@ class LibraryBook(models.Model):
     isbn = fields.Char(string='ISBN')
     active = fields.Boolean(string='Active?', default=True)
     description = fields.Text(string='Description')
+
     author_id = fields.Many2one('library.author', string='Author')
     category_ids = fields.Many2many('library.book.category', string='Categories')
+    
     borrow_date = fields.Date(string='Borrow Date', readonly=True)
     due_date = fields.Date(string='Due Date', compute='_compute_due_date', readonly=True)
 
@@ -26,16 +28,13 @@ class LibraryBook(models.Model):
     def _compute_due_date(self):
         for book in self:
             if book.borrow_date:
-                # Tutaq ki, kitab 14 gün müddətinə verilir
                 book.due_date = book.borrow_date + timedelta(days=7)
             else:
-                book.due_date = False # Əgər icarə tarixi yoxdursa, son tarix də olmasın
+                book.due_date = False 
 
     @api.onchange('author_id')
     def _onchange_author(self):
-        # Əgər bir yazıçı seçilibsə və onun doğum tarixi varsa
         if self.author_id and self.author_id.birth_date:
-            # Xəbərdarlıq mesajı qaytar
             return {
                 'warning': {
                     'title': "Məlumat",
@@ -46,18 +45,19 @@ class LibraryBook(models.Model):
     def make_borrowed(self):
         self.write({
             'state': 'borrowed',
-            'borrow_date': fields.Date.today() # Bugunun tarixini yazır
+            'borrow_date': fields.Date.today() 
         })
 
     def make_available(self):
         self.write({
             'state': 'available',
-            'borrow_date': False # Tarixi təmizləyir
+            'borrow_date': False 
         })
 
     def make_lost(self):
         self.write({'state': 'lost'})
 
+    # Eyni anda bir nece qeydi yarada biler
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
--- library_management/models/library_book_category.py
+++ library_management/models/library_book_category.py
@@ -5,3 +5,9 @@ class LibraryBookCategory(models.Model):
     _description = 'Library Book Category'
 
     name = fields.Char(string='Name', required=True)
+
+    category_book_ids = fields.One2many(
+        'library.book',      # Əlaqəli model (hansı modelin obyektlərini göstərəcək)
+        'category_ids',         # Əlaqəli modeldəki geriyə-əlaqə sahəsi (bizim Many2one sahəmiz)
+        string='Books'       # Sahənin interfeysdəki adı
+    )
