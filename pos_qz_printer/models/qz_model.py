d8079824e8a1119ada3caea140099498cfc4bea6
Aim <7asim6@gmail.com>
2025-07-14 09:24:01 +0400

    Delete action_print_receipt

--- pos_qz_printer/models/qz_model.py
+++ pos_qz_printer/models/qz_model.py
@@ -6,13 +6,3 @@ class QzModel(models.Model):
 
     name = fields.Char(string='Name', required=True, default="Test Product")
     total = fields.Float(string='Total', default=15.75)
-
-    def action_print_receipt(self):
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'print_qz_receipt',
-            'params': {
-                'name': self.name,
-                'total': self.total,
-            }
-        }
